ea3bb734d14a79862e9036645a8f5b5b0684d0a4
Asrin Yagmur <auyagmur@hotmail.com>
2024-05-26 17:02:30 +0300

    sign in button, sign-up redirect

--- app/(auth)/sign-in.jsx
+++ app/(auth)/sign-in.jsx
@@ -5,11 +5,18 @@ import { images } from "../../constants";
 
 import FormField from "../../components/FormField";
 
+import { CustomButton } from "../../components/CustomButton";
+import { Link } from "expo-router";
+
 const SignIn = () => {
   const [signInForm, setSignInForm] = useState({
     email: "",
     password: "",
   });
+
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  const handleSignIn = () => {};
 
   return (
     <SafeAreaView className="bg-primary h-full">
@@ -21,10 +28,7 @@ const SignIn = () => {
             className="w-[145px] h-[92px]"
           />
 
-          <Text
-            className="text-bold text-xl font-psemibold mt-2 text-white"
-            onPress={() => console.log("1234")}
-          >
+          <Text className="text-bold text-xl font-psemibold mt-2 text-white">
             Giriş Yapın
           </Text>
 
@@ -39,8 +43,26 @@ const SignIn = () => {
           <FormField
             title="Password"
             value={signInForm.password}
-            handleChangeText={(e) => setSignInForm({ ...signInForm, password: e })}
+            handleChangeText={(e) =>
+              setSignInForm({ ...signInForm, password: e })
+            }
             otherStyles="mt-7"
+          />
+
+          <View className="justify-center pt-5 flex-row">
+            <Link
+              href="/sign-up"
+              className="text-secondary text-lg font-psemibold"
+            >
+              Hesabınız Yok mu ?
+            </Link>
+          </View>
+
+          <CustomButton
+            title="Giriş Yap"
+            handlePress={handleSignIn}
+            containerStyles="mt-7"
+            isLoading={isSubmitting}
           />
         </View>
       </ScrollView>
